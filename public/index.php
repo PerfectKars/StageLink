b581b3371040d9d2b990e04bcbbf80b49f5dd8fd
Priam <priam683@gmail.com>
2026-04-01 22:56:52 +0200

    style: modifications locales avant merge develop

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 // Chargement .env
-$envFile = dirname(__DIR__, 2) . '/.env';
+$envFile = dirname(__DIR__) . '/.env';
 if (file_exists($envFile)) {
     foreach (file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) as $line) {
         if (str_starts_with(trim($line), '#')) continue;
